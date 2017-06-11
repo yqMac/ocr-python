7ab0b6af73a143c3c7013703be2d3eecf620deb2
rookie <yqmac163@163.com>
2017-06-11 14:58:57 +0800

    简化训练配置。

--- model/parse_args.py
+++ model/parse_args.py
@@ -4,25 +4,27 @@ import sys
 
 def parse_arg():
     parser = argparse.ArgumentParser()
-    parser.add_argument("TrainingDirc", help="Path to training data directory")
-    parser.add_argument("ValidateDirc", help="Path to Validation data file")
-    parser.add_argument("TestDirc", help="Path to test data file")
-    parser.add_argument("ModelParamsFile",
-                        help="Path to the directory where params is to be stored followed by prefix for the parameter file")
+
+    parser.add_argument("TrainingModeId", help="The id created when training")
+    parser.add_argument("-img_height", "--img_height", metavar='\b', help="img_height", type=int, default=70)
+    parser.add_argument("-img_width", "--img_width", metavar='\b', help="img_width", type=int, default=160)
+    parser.add_argument("-length", "--length", help="Length of the characters", metavar='\b', type=int, default=5)
+    parser.add_argument("-ResultPre", help="stored result with this file", default="lstm_")
+
+    parser.add_argument("-batchsize", "--batchsize", type=int, default=500, help="Training batch size", metavar='\b')
+    parser.add_argument("-testsize", "--testsize", type=int, default=500, help="Test batch size", metavar='\b')
+
     parser.add_argument("-maxsoft", "--maxsoft", help="Provide this argument if you want to run maxsoft", metavar='\b')
     parser.add_argument("-bidirec", "--bidirec", help="Provide this argument in order to run bidirectional lstm",
                         action="store_true")
     parser.add_argument("-hiddenlayers", "--hiddenlayers", help="Number of hidden layers in the network", metavar='\b')
     parser.add_argument("-learningrate", "--learningrate", help="Learning rate", metavar='\b')
-    parser.add_argument("-batchsize", "--batchsize", help="Training batch size", metavar='\b')
-    parser.add_argument("-testsize", "--testsize", help="Test batch size", metavar='\b')
-    parser.add_argument("-includeCapital", "--includeCapital", help="Include capital letters or not",action="store_true")
-    parser.add_argument("-length", "--length", help="Length of the characters", metavar='\b')
+    parser.add_argument("-includeCapital", "--includeCapital", help="Include capital letters or not",
+                        action="store_true")
     parser.add_argument("-rescale_in_preprocessing", "--rescale", help="Rescale_in_preprocessing", action="store_true")
     parser.add_argument("-use_mask_input", "--use_mask_input", help="Use_mask_input", action="store_true")
-    parser.add_argument("-lstm_layer_units", "--lstm_layer_units", metavar='\b', help="No of units in lstm layer", type=int, default=256)
-    parser.add_argument("-img_height", "--img_height", metavar='\b', help="img_height", type=int, default=70)
-    parser.add_argument("-img_width", "--img_width", metavar='\b', help="img_width", type=int, default=160)
+    parser.add_argument("-lstm_layer_units", "--lstm_layer_units", metavar='\b', help="No of units in lstm layer",
+                        type=int, default=256)
     parser.add_argument("-cnn_dense_layer_sizes", "--cnn_dense_layer_sizes", metavar='\b',
                         help="No of units in dense layer", type=int, default=256)
     parser.add_argument("-multichar", "--multichar", help="multichar disabled", action="store_false")
---
+++ training_data_gen/train_parse_args.py
@@ -0,0 +1,15 @@
+import argparse
+import sys
+
+
+def train_parse_arg():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("image_dir", help="Path where captcha is ")
+    parser.add_argument("max_size",type=int, help="Num of captcha to save as one model")
+    parser.add_argument("length",type=int, help="Length of the characters")
+    parser.add_argument("-ignore_case", help="if case sensitive",default=False)
+    parser.add_argument("-captcha_pattern", help="capthcha filename pattern",default="^\d+_(.*)\..+$")
+    args = parser.parse_args()
+    print "Params passed are : "
+    print args
+    return args
