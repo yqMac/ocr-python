f42707d08eb89bde366441b5ba23d07e3d56b865
rookie <yqmac163@163.com>
2018-05-18 08:25:38 +0800

    txn_3.commit()

--- crnn_pytorch/dataset.py
+++ crnn_pytorch/dataset.py
@@ -118,6 +118,7 @@ def merge_lmdb(result_lmdb, lmdb2):
 
     # 更新大小
     txn_3.put("num-samples", str(count_total))
+    txn_3.commit()
     # 输出结果lmdb的状态信息，可以看到数据是否合并成功
     print(env_3.stat())
     # 关闭lmdb
