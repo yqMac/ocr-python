f15441318cf134752ef849d25e777093b4b82e57
rookie <yqmac163@163.com>
2018-05-26 01:28:48 +0800

    max_iter_num

--- crnn_pytorch/crnn_main_plus.py
+++ crnn_pytorch/crnn_main_plus.py
@@ -286,7 +286,7 @@ else:
     optimizer = optim.RMSprop(crnn.parameters(), lr=opt.lr)
 
 
-def val(crnn, val_data_list_param, criterion, max_iter=100):
+def val(crnn, val_data_list_param, criterion, max_iter_num=100):
     # print('开始校验准确性')
     for p in crnn.parameters():
         p.requires_grad = False
@@ -311,7 +311,7 @@ def val(crnn, val_data_list_param, criterion, max_iter=100):
         one_correct = 0
         loss_avg = utils.averager()
         # 检测所用的图片数量
-        max_iter = min(max_iter, len(data_loader))
+        max_iter = min(max_iter_num, len(data_loader))
         # 检测的总数量增加
         all_Count += max_iter * opt.batchSize
 
