900a5872c14323aa3f200353885b3caafc002b57
yqmac <yqmac163@163.com>
2018-05-28 10:18:06 +0800

    try to split

---
+++ crnn_pytorch/tool/append_dataset.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+from __future__ import print_function
+from __future__ import print_function
+from __future__ import print_function
+
+import argparse
+import logging
+import lmdb
+import os
+
+from rookie_utils.Logger import Logger
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--result', required=False, help='path to result')
+parser.add_argument('--src', required=False, help='path to resource')
+opt = parser.parse_args()
+print(opt)
+
+
+# read lmdb2 data then write into lmdb1
+def merge_lmdb(result_lmdb, lmdb2, max_size=-1, logger=None):
+    # env代表Environment, txn代表Transaction
+    print('Merge start!')
+    # 打开lmdb文件，读模式
+    env_2 = lmdb.open(lmdb2)
+    # 创建事务
+    txn_2 = env_2.begin()
+
+    count_2 = txn_2.get('num-samples')
+
+    # 打开数据库
+    database_2 = txn_2.cursor()
+
+    # 打开lmdb文件，写模式，
+    env_3 = lmdb.open(result_lmdb, map_size=int(1e12))
+    txn_3 = env_3.begin(write=True)
+    count_3 = txn_3.get('num-samples')
+    if count_3 is None:
+        count_3 = '0'
+    count_2 = int(count_2)
+    count_3 = int(count_3)
+    if max_size == -1 or max_size >= count_2:
+        max_size = count_2
+
+    count_total = max_size + count_3
+    if logger != None:
+        logger("数据集:{},总:{},处理:{},整合后:{}".format(lmdb2, count_2, max_size, count_total))
+    count = 1
+    # 遍历数据库
+    while count <= max_size:
+        image_key = "image-%09d" % (count)
+        lable_key = "label-%09d" % (count)
+        new_image_key = "image-%09d" % (count_3 + count)
+        new_lable_key = "label-%09d" % (count_3 + count)
+        txn_3.put(new_image_key, txn_2.get(image_key))
+        txn_3.put(new_lable_key, txn_2.get(lable_key))
+        if count % 1000 == 0:
+            print("Merge: {}".format(count))
+            txn_3.commit()
+            txn_3 = env_3.begin(write=True)
+        count += 1
+    if count % 1000 != 0:
+        txn_3.commit()
+        txn_3 = env_3.begin(write=True)
+
+    # 更新大小
+    txn_3.put("num-samples", str(count_total))
+    txn_3.commit()
+    # 输出结果lmdb的状态信息，可以看到数据是否合并成功
+    res = env_3.stat()
+    print(res)
+    # 关闭lmdb
+    env_2.close()
+    env_3.close()
+    print('Merge success! count: {}'.format(count))
+    return res
+
+
+
+# 日志输出
+log_path = "../../logs/append.log"
+if not os.path.exists(os.path.dirname(log_path)):
+    os.makedirs(os.path.dirname(log_path))
+logger = Logger(log_path, logging.INFO, logging.INFO)
+
+
+def print_msg(msg):
+    logger.info(msg)
+
+
+if __name__ == '__main__':
+    if (opt.result is None or opt.result == '') and (opt.src is None or opt.src == ''):
+        raise Exception('imagePath and iamgeDirPath must not to be both blank')
+    print_msg("将要吧{}的lmdb附加到{}".format(opt.src,opt.result))
+    merge_lmdb(opt.result, opt.src, -1, print_msg)
---
+++ crnn_pytorch/tool/create_lmdb.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python
+# encoding: utf-8
+# coding:utf-8
+
+import argparse
+import logging
+import os
+import re
+from glob import glob
+import random
+
+import cv2
+import lmdb  # install lmdb by "pip install lmdb"
+import numpy as np
+
+from rookie_utils.Logger import Logger
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--imagePath', required=False, help='path to image')
+parser.add_argument('--lmdbPath', required=False, help='path to lmdb')
+parser.add_argument('--head', required=False, help='file name pre to save')
+parser.add_argument('--regex', required=False, default="^.*_(.*)\..+$", help='parse file regex with group 1')
+opt = parser.parse_args()
+print(opt)
+
+'''
+创建流程:
+1、imagePath和imageDirPath 不能同时为空 前者是单数据集情况下的数据目录，后者是多训练集情况下所有数据集目录的文件夹
+2、程序会读取所有目录下的符合正则的文件，写到lmdbPath下，以head(默认为数据集目录名)为文件夹名
+
+'''
+
+
+def checkImageIsValid(imageBin):
+    if imageBin is None:
+        return False
+    imageBuf = np.fromstring(imageBin, dtype=np.uint8)
+    img = cv2.imdecode(imageBuf, cv2.IMREAD_GRAYSCALE)
+    imgH, imgW = img.shape[0], img.shape[1]
+    if imgH * imgW == 0:
+        return False
+    return True
+
+
+def writeCache(env, cache):
+    with env.begin(write=True) as txn:
+        for k, v in cache.items():
+            txn.put(k, v)
+
+
+def createDataset(outputPath, imagePathList, outputHead, regexStr, checkValid=True):
+    """
+    Create LMDB dataset for CRNN training.
+    split the imagesList to ten parts, nine for train, one for val
+    ARGS:
+        outputPath    : LMDB output path
+        imagePathList : list of image path
+        labelList     : list of corresponding groundtruth texts
+        checkValid    : if true, check the validity of every image
+    """
+    # assert (len(imagePathList) == len(labelList))
+    nSamples = len(imagePathList)
+    print(nSamples)
+    train_size = nSamples;
+    # train_size = int(round(round(nSamples / 10000.0, 1) * 9, 0) * 1000)
+    print(train_size)
+    #val_size = nSamples - train_size
+    #print(val_size)
+    if outputPath is None or outputPath == "":
+        file_path = os.path.dirname(os.path.realpath(__file__))
+        crnn_path = os.path.dirname(file_path)
+        datasets_path = crnn_path + '/datasets'
+        outputPath = datasets_path
+    train_lmdb_path = outputPath + "/" + outputHead
+    if not os.path.exists(train_lmdb_path):
+        os.makedirs(train_lmdb_path)
+    env_train = lmdb.open(train_lmdb_path, map_size=1099511627776)
+    cache = {}
+    cnt = 1
+    for i in range(nSamples):
+        imagePath = imagePathList[i]
+        try:
+            match = re.compile(regexStr).match(imagePath.split("/")[-1])
+            # match = re.compile("^(.*)\..+$").match(imagePath.split("/")[-1])
+            label = match.group(1)
+            if not os.path.exists(imagePath):
+                print('%s does not exist' % imagePath)
+                continue
+            with open(imagePath, 'r') as f:
+                imageBin = f.read()
+            if checkValid:
+                if not checkImageIsValid(imageBin):
+                    print('%s is not a valid image' % imagePath)
+                    continue
+
+            imageKey = 'image-%09d' % cnt
+            labelKey = 'label-%09d' % cnt
+            cache[imageKey] = imageBin
+            cache[labelKey] = label
+            if (i + 1) % 1000 == 0:
+                writeCache(env_train, cache)
+                print('Written train %d / %d' % (cnt, train_size))
+                cache = {}
+                if cnt == train_size:
+                    cnt = 0
+
+            cnt += 1
+        except Exception as e:
+            print("the image {0} is error{1}".format(imagePath, e.message))
+    cache['num-samples'] = str(train_size)
+    writeCache(env_train, cache)
+    # print('Created val dataset with %d samples' % val_size)
+    # cache.clear()
+    # cache['num-samples'] = str(train_size)
+    # writeCache(env_train, cache)
+    print('Created train dataset with %d samples' % train_size)
+
+if __name__ == '__main__':
+    if (opt.imagePath is None or opt.imagePath == '') and (opt.imageDirPath is None or opt.imageDirPath == ''):
+        raise Exception('imagePath and iamgeDirPath must not to be both blank')
+    paths = glob(opt.imagePath + "/*.*")
+    print("split the imagePathList to ten parts, nine for train, one for val")
+    random.shuffle(paths)
+    createDataset(opt.lmdbPath, paths, opt.head, opt.regex)
